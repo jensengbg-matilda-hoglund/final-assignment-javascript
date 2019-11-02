47ccd2decc790ecb57ef7433a926a169fd992040
Matilda Höglund <hoglund.matilda@gmail.com>
2019-11-02 17:29:14 +0100

    test

--- js/index.js
+++ js/index.js
@@ -1,44 +1,62 @@
-// ------- JSON data -> image display ------- //
-let imgArray = [];
+/* SEARCH FOR IMAGE IN INPUTFIELD AND DISPLY ON WEBSITE */
 
-//https://www.flickr.com/photos/{user-id}/{photo-id} - individual photo
+// -------------- Making API URL ------------ //
 
-// ---------------------------------------------- //
+const api_key = "19d3e6e0acfe9c438f368e2c2bab1c5d"; // flickr-key
+let quantity = document.getElementById("desired-quantity"); // number of imges shown depending on user (DO LATER)
+const userSearch = document.getElementById("search-field"); // user input search
 
-// ---- SEARCH FUNCTION ---- //
-
-const api_key = "&api_key=19d3e6e0acfe9c438f368e2c2bab1c5d"; // flickr-key
-let quantity = "5"; // number of imges shown
-const pages = "&page=1"; // Number of pages
-const userSearch = document.getElementById("search-field"); // input search
-
+// https:// flickr rest api method - search - photo only - one or more tags -
 async function getData() {
-  const URL = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api_key}&per_page=${quantity}&tags=${encodeURIComponent(
+  urlArray = [];
+  const URL = `https://www.flickr.com/services/rest/?method=flickr.photos.search&media=photos&per_page=${encodeURIComponent(
+    quantity.value
+  )}&tag_mode=all&api_key=${api_key}&sort=relevance&tags=${encodeURIComponent(
     userSearch.value
-  )}&format=json`;
+  )}&format=json&nojsoncallback=1`;
   let response = await fetch(URL, { method: "GET" });
-  let data = await response;
-  console.log(data);
-  return await data; // SOME SHITTY PROBLEM HERE?
+  let data = await response.json();
+  let object = await data; // Whole JSON object, dont need that crap soo ->
+  let imgArray = object.photos.photo; // ... I took the things I actually need, like the array with all photo's.
+  console.log(imgArray); // In console we can se all the images as objects with their attributes.
+  createURL(imgArray); // Now lets pass this array into the next function createURL.
 }
 
 const searchButton = document.getElementById("search-button");
 searchButton.addEventListener("click", getData);
 
-// ---------------------------------------------------- //
+// ---------------------------------------- //
 
-// DISPLAY GALLERY
-/*const gallery = document.getElementById("flickr-gallery");
+// -------------- MAKING URL'S----------- //
 
-async function display() {
-  let output = await getData();
+// To show an image on the page I need an URL to use as SRC in <img> tag.
+// I use .map() to loop through our imgArray and take out the necesary info put it in the URL-formula.
+// With .map() this is autmatically done for each element in the array, and return's a new array with URL's.
 
-  gallery.innerHTML = output;
-}
+createURL = imgArray => {
+  let urlArray = imgArray.map(index => {
+    return `https://farm${index.farm}.staticflickr.com/${index.server}/${index.id}_${index.secret}.jpg`;
+  });
 
-// To map <photo> elements to urls, please read the url documentation.
-// https://www.flickr.com/services/api/misc.urls.html
+  createImage(urlArray); // Sending this variable to the next function
+  console.log(urlArray); // Look in the console to see if our URL's look's like we want them to.
+};
+// ----------------------------------------- //
 
+// ----------- DISPLAY GALLERY ------------ //
+const gallerySection = document.getElementById("flickr-gallery"); // GLOBAL varable
+
+createImage = urlArray => {
+  for (let i = 0; i < urlArray.length; i++) {
+    const newImage = document.createElement("img"); // create new img element for each element int the array
+    newImage.src = urlArray[i]; // give the element the src of [i]
+    newImage.width = "150px";
+    newImage.className = "image"; // classname for styling it in css
+    gallerySection.appendChild(newImage); // add it inside of "gallerySection"
+  }
+};
+
+// ------------------ THE END ------------- //
 /*
 ------- USER choose mount of images to show -----
 - input-field number, or checkboxes
