672238256b9dd4efac90219575481671d3fb9cd9
Matilda Höglund <hoglund.matilda@gmail.com>
2019-11-04 01:19:00 +0100

    mobile layout

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
-/* SEARCH FOR IMAGE IN INPUTFIELD AND DISPLY ON WEBSITE */
+/*
+-------------- ATT GÖRA -------------
+* Söka på bilder med ESC-knapp
+* Varje ny sökning ska nollställa resultatet från förra sökningen
+*
+*/
 
-// -------------- Making API URL ------------ //
-
+// API-URL (hämta all data från flickR)
 const api_key = "19d3e6e0acfe9c438f368e2c2bab1c5d"; // flickr-key
-let quantity = document.getElementById("desired-quantity"); // number of imges shown depending on user (DO LATER)
-const userSearch = document.getElementById("search-field"); // user input search
+const userSearch = document.getElementById("search-field"); // user input search (tags)
 
-// https:// flickr rest api method - search - photo only - one or more tags -
 async function getData() {
-  urlArray = [];
-  const URL = `https://www.flickr.com/services/rest/?method=flickr.photos.search&media=photos&per_page=${encodeURIComponent(
-    quantity.value
-  )}&tag_mode=all&api_key=${api_key}&sort=relevance&tags=${encodeURIComponent(
+  // RESET BOTH ARRAYS AFTER IMAGES ARE DISPLAYED IN GALLERY SECTION
+  const URL = `https://www.flickr.com/services/rest/?method=flickr.photos.search&media=photos&per_page=50&sort=interestingness_desc&page=8&tag_mode=all&api_key=${api_key}&sort=relevance&tags=${encodeURIComponent(
     userSearch.value
   )}&format=json&nojsoncallback=1`;
   let response = await fetch(URL, { method: "GET" });
@@ -22,18 +22,15 @@ async function getData() {
   createURL(imgArray); // Now lets pass this array into the next function createURL.
 }
 
-const searchButton = document.getElementById("search-button");
-searchButton.addEventListener("click", getData);
-
-// ---------------------------------------- //
-
-// -------------- MAKING URL'S----------- //
-
 // To show an image on the page I need an URL to use as SRC in <img> tag.
 // I use .map() to loop through our imgArray and take out the necesary info put it in the URL-formula.
 // With .map() this is autmatically done for each element in the array, and return's a new array with URL's.
 
+// MAKING URL from API data
 createURL = imgArray => {
+  document.querySelectorAll(".img-container").forEach(n => n.remove());
+  document.querySelectorAll(".gallery-img").forEach(n => n.remove());
+
   let urlArray = imgArray.map(index => {
     return `https://farm${index.farm}.staticflickr.com/${index.server}/${index.id}_${index.secret}.jpg`;
   });
@@ -43,18 +40,44 @@ createURL = imgArray => {
 };
 // ----------------------------------------- //
 
-// ----------- DISPLAY GALLERY ------------ //
+// Search-button
+const searchButton = document.getElementById("search-button");
+searchButton.addEventListener("click", getData);
+
+//Tryck ENTER för att söka
+const enterSearch = document.getElementById("search-field");
+enterSearch.addEventListener("keydown", function(e) {
+  if (e.keyCode === 13) {
+    getData();
+  }
+});
+
+// DISPLAY GALLERY
 const gallerySection = document.getElementById("flickr-gallery"); // GLOBAL varable
 
 createImage = urlArray => {
   for (let i = 0; i < urlArray.length; i++) {
-    const newImage = document.createElement("img"); // create new img element for each element int the array
-    newImage.src = urlArray[i]; // give the element the src of [i]
-    newImage.width = "150px";
-    newImage.className = "image"; // classname for styling it in css
-    gallerySection.appendChild(newImage); // add it inside of "gallerySection"
+    const newDiv = document.createElement("div"); // create new div element for each URL
+    const newImage = document.createElement("img"); // -||- img element
+    newDiv.className = "img-container"; // give div classname
+    gallerySection.appendChild(newDiv); // append div to my gallery-section
+    newImage.src = urlArray[i]; // give img an SRC from URL i created earlier
+
+    newImage.className = "gallery-img"; // give img classname
+    newDiv.appendChild(newImage); // append img to div.
+    // --- REAPEAT for each element in my URL array --- //
   }
+  imgArray = [];
+  urlArray = [];
 };
+
+// RESET SEARCHFIELD WHIT X-button
+resetSearch = () => {
+  userSearch.value = "";
+};
+
+const clearInput = document.getElementById("reset-search");
+clearInput.addEventListener("click", resetSearch);
 
 // ------------------ THE END ------------- //
 /*
@@ -65,8 +88,8 @@ createImage = urlArray => {
 
 /* 
 ---------- LIGHTBOX ----------
-- Onclikc open lightbox,
-- cross field toclose, and maybe with esc button aswell.
+- Onclick open lightbox,
+- cross field toclose, linked to esc button.
 */
 
 /* 
@@ -74,3 +97,7 @@ createImage = urlArray => {
  -  Show related tags
  -  Patricle JS background in header/hero section
  */
+
+/* ------------ GALLERI TEST --------------- */
+
+//let gallery = document.getElementsByClassName("gallery")[0];
