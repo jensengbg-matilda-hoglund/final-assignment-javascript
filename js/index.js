3f0270de2a023990cab154ca3f11ea25b7f5263f
Matilda Höglund <hoglund.matilda@gmail.com>
2019-11-05 02:40:41 +0100

    filter size & image/page

--- js/index.js
+++ js/index.js
@@ -1,56 +1,62 @@
 /*
 -------------- ATT GÖRA -------------
 * LIGHTBOX
-* FILTER (images/page) ---- / extra: color, size, style
-*
+* FILTER (images/page) ---- / extra: color, size.
+* STYLA bilderna så det blir space mellan eller inga rader.
+*/
+
+/*
+IMG SIZES
+- small = 
 */
 
 // API-URL (hämta all data från flickR)
-const api_key = "19d3e6e0acfe9c438f368e2c2bab1c5d"; // flickr-key
-const userSearch = document.getElementById("search-field"); // user input search (tags)
+const api_key = "19d3e6e0acfe9c438f368e2c2bab1c5d"; // my flickR API-key
+const userSearch = document.getElementById("search-field"); // user input search
+const quantity = document.getElementById("number-of-images"); // user images select
+const imageSize = document.getElementById("image-size");
 
 async function getData() {
   // RESET BOTH ARRAYS AFTER IMAGES ARE DISPLAYED IN GALLERY SECTION
-  const URL = `https://www.flickr.com/services/rest/?method=flickr.photos.search&media=photos&per_page=&sort=relevance&page=1&tag_mode=all&api_key=${api_key}&sort=relevance&tags=${encodeURIComponent(
+  const URL = `https://www.flickr.com/services/rest/?method=flickr.photos.search&m
+  edia=photos&per_page=${encodeURIComponent(
+    quantity.value
+  )}&sort=relevance&page=1&tag_mode=all&api_key=${api_key}&sort=relevance&tags=${encodeURIComponent(
     userSearch.value
   )}&format=json&nojsoncallback=1`;
   let response = await fetch(URL, { method: "GET" });
   let data = await response.json();
-  let object = await data; // Whole JSON object, dont need that crap soo ->
-  let imgArray = object.photos.photo; // ... I took the things I actually need, like the array with all photo's.
-  console.log(imgArray); // In console we can se all the images as objects with their attributes.
+  let object = await data; // Whole JSON object, dont need that crap soo -->>
+  let imgArray = object.photos.photo; // ... let's only take info about the image which is stored as an array.
   createURL(imgArray); // Now lets pass this array into the next function createURL.
 }
 
-// To show an image on the page I need an URL to use as SRC in <img> tag.
-// I use .map() to loop through our imgArray and take out the necesary info put it in the URL-formula.
-// With .map() this is autmatically done for each element in the array, and return's a new array with URL's.
-
 // MAKING URL from API data
 createURL = imgArray => {
-  document.querySelectorAll(".img-container").forEach(n => n.remove());
+  document.querySelectorAll(".img-container").forEach(n => n.remove()); // Remove previous IMG & DIV elements
   document.querySelectorAll(".gallery-img").forEach(n => n.remove());
 
   let urlArray = imgArray.map(index => {
-    return `https://farm${index.farm}.staticflickr.com/${index.server}/${index.id}_${index.secret}.jpg`;
+    return `https://farm${index.farm}.staticflickr.com/${index.server}/${index.id}_${index.secret}_${imageSize.value}.jpg`;
   });
 
   createImage(urlArray); // Sending this variable to the next function
   console.log(urlArray); // Look in the console to see if our URL's look's like we want them to.
 };
-// ----------------------------------------- //
+// ---------------------------------------------------- //
 
 // Search-button
 document.getElementById("search-button").addEventListener("click", getData);
-
-// ENTER-knapp
+// Enter-KEY search
 document
   .getElementById("search-field")
-  .addEventListener("keydown", function(e) {
-    if (e.keyCode === 13) {
+  .addEventListener("keydown", function(event) {
+    if (event.keyCode === 13) {
       getData();
     }
   });
+
+//-------------------------------------------------------//
 
 // DISPLAY GALLERY
 const gallerySection = document.getElementById("flickr-gallery"); // GLOBAL varable
@@ -62,9 +68,13 @@ createImage = urlArray => {
     newDiv.className = "img-container"; // give div classname
     gallerySection.appendChild(newDiv); // append div to my gallery-section
     newImage.src = urlArray[i]; // give img an SRC from URL i created earlier
-
     newImage.className = "gallery-img"; // give img classname
-    newDiv.appendChild(newImage); // append img to div.
+    newDiv.appendChild(newImage);
+    newImage.addEventListener("click", function(event) {
+      lightbox(event);
+    });
+
+    //newImage.addEventListener("click", function(e) { console.log(e); });
     // --- REAPEAT for each element in my URL array --- //
   }
   imgArray = [];
@@ -77,17 +87,35 @@ document.getElementById("reset-search").addEventListener("click", function(e) {
 });
 
 // ------------------ THE FUCKING END OF MY SUPER FUCKING AWESOME CODE ------------- //
-/*
-------- USER choose mount of images to show -----
-- input-field number, or checkboxes
-- change the variable
-*/
 
 /* 
 ---------- LIGHTBOX ----------
-- Onclick open lightbox,
-- cross field toclose, linked to esc button.
+- Click on image                                                          %
+- Start lightbox function                                                 %
+- Function locate URL from the clicked IMG                                %
+- Function "sends" URL to element in HTML                                 %
+- Element in HTML changes from "none" to flex.                            %
+- add buttons/icons in overlay to display in fron of img, z-index 2.      %
+- style IMG in html-lightbox
+- Make buttons in html element work ex: next, previous, close/esc
+- close button make element display: none again
+- next/previous WTFi dont know
 */
+
+const overlay = document.getElementById("lightbox-overlay");
+const lightboxImage = document.getElementById("lightbox-image");
+
+lightbox = event => {
+  lightboxImage.src = event.srcElement.attributes[0].value;
+  overlay.appendChild(lightboxImage);
+  overlay.style.display = "flex";
+};
+
+/*
+        <section class="overlay">
+          <img id="lightbox-image" src="" alt="" />
+        </section>
+        */
 
 /* 
  --------- EXTRA ---------
